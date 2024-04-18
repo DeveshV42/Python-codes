de2a51efa20c383e919990016892d2ab3fbbfac9
Devesh Vashistha <devesh.vashistha@inforcea.com>
2024-04-18 21:30:44 +0530

    commit

---
+++ calculatoroperationmodule1.py
@@ -0,0 +1,10 @@
+def addition(a,b):
+    return a+b
+
+def substraction(a,b):
+    return a-b
+
+def division(a,b):
+    return a/b
+
+    #Go to the operations module2.py file
---
+++ dictionary.py
@@ -0,0 +1,16 @@
+students={'name':"dev",'age':20}
+print(students.keys())
+
+print(students['name'])
+
+print(students.items())
+
+students['email']='deveshvashishtha42@gmail.com'
+print(students)
+
+print(students.pop('age'))
+print(students)
+
+for i in students.items():
+    print(i)
+
